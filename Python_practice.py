a987a38e4f16360cee2fa96950b023a73ad1ea27
Mikhail Tikhanchikov <78825344+mmtikhan@users.noreply.github.com>
2022-04-28 12:42:42 -0700

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,51 @@
+"""counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+'''print(counties_dict)
+print(len(counties_dict))
+print(counties_dict.items())
+print(counties_dict.keys())
+print(counties_dict.values())
+print(counties_dict.get("Denver"))
+print(counties_dict['Arapahoe'])'''
+voting_data = []
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+print(voting_data)"""
+
+'''counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])'''
+    #What is the score?
+"""score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')"""
+'''counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")'''
+'''# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)'''
+import csv
+dir(csv)
+    
